6220448a1b92f8ea7e646f8b02158f8396086a46
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2017-12-21 20:05:43 -0600

    Página Principal lista
    Se ha hecho la página home.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ROUTES } from './app.routing';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MDBBootstrapModule.forRoot(),
+    RouterModule.forRoot(ROUTES)
   ],
+  schemas: [ NO_ERRORS_SCHEMA ],
   providers: [],
   bootstrap: [AppComponent]
 })
