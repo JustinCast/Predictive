81ed1a1606488d35ceecf53c0baac4961a0acbad
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2017-12-23 16:49:15 -0600

    'Zona Predictiva' en proceso
    Se ha hecho un componente que tiene como nombre 'Predict', es el mismo es donde se encontrará la zona predictiva del proyecto.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { RouterModule } from "@angular/router";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatCardModule,
+} from "@angular/material";
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ROUTES } from './app.routing';
+import { PredictComponent } from './predict/predict.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PredictComponent
   ],
   imports: [
     BrowserModule,
     MDBBootstrapModule.forRoot(),
-    RouterModule.forRoot(ROUTES)
+    RouterModule.forRoot(ROUTES),
+    BrowserAnimationsModule,
   ],
   schemas: [ NO_ERRORS_SCHEMA ],
   providers: [],
